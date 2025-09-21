ee7b4364eec931993e5b64b8459b67346b71d516
Tristan Zybala <tzybala@usf.edu>
2025-09-21 10:37:26 -0400

    Create array of objects

--- cc_4.js
+++ cc_4.js
@@ -1,1 +1,10 @@
 // Coding Challenge 04
+
+// Step 2: Create array
+let products = [
+    {name: "pasta",category: "dry grocery",price: 1.99,inventory: 65},
+    {name: "bread",category: "dry grocery",price: 2.99,inventory: 37},
+    {name: "toothpaste",category: "oral hygiene",price: 2.79,inventory: 22},
+    {name: "milk",category: "dairy",price: 3.99,inventory: 18},
+    {name: "ground chuck",category: "meats",price: 6.75,inventory: 15}
+];
