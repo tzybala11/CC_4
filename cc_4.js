4187a479932e537e922492ac371225c328ce098b
Tristan Zybala <tzybala@usf.edu>
2025-09-21 20:18:15 -0400

    Logging Key/Value Pairs

--- cc_4.js
+++ cc_4.js
@@ -2,10 +2,10 @@
 
 // Step 2: Create array
 let products = [
-    {name: "pasta",category: "dry grocery",price: 1.99,inventory: 65},
-    {name: "bread",category: "dry grocery",price: 2.99,inventory: 37},
-    {name: "toothpaste",category: "oral hygiene",price: 2.79,inventory: 22},
-    {name: "milk",category: "dairy",price: 3.99,inventory: 18},
+    {name: "pasta",category: "groceries",price: 1.99,inventory: 65},
+    {name: "bread",category: "groceries",price: 2.99,inventory: 37},
+    {name: "shirt",category: "apparel",price: 14.99,inventory: 12},
+    {name: "headphones",category: "electronics",price: 34.99,inventory: 18},
     {name: "ground chuck",category: "meats",price: 6.75,inventory: 15}
 ];
 
@@ -13,40 +13,48 @@ let products = [
 for (const product of products) {
     let discount = 0;
     switch (product.category) {
-        case "dry grocery":
-            discount = 0.12
+        case "electronics":
+            discount = 0.20;
             break;
-        case "oral hygiene":
-            discount = 0.10
+        case "apparel":
+            discount = 0.15;
             break;
-        case "dairy":
-            discount = 0.08
-            break;
-        case "meats":
-            discount = 0.05
+        case "groceries":
+        case "household":
+            discount = 0.10;
             break;
         default:
             break;
     }
     // Discounted Unit Price
-    let promoPrice = product.price * (1-discount);
-    product.promoPrice = promoPrice;
+    product.promoPrice = (product.price * (1-discount)).toFixed(2);
 };
 
 console.log(products);
 
 //Step 4: Customer Discounts
 const customerType = ["member","silver","gold","platinum"]; //Member: 0%, Silver: 2%, Gold: 5%, Platinum: 10%
+let extraDiscount = 0
 if (customerType === "platinum") {
-    total = subTotal * 0.10;
-    console.log(`Thank you for being a Platinum member! A 10% discount has been applied to your transaction: ${total}`);
+    extraDiscount = 0.10;
+    console.log(`Thank you for being a Platinum member! A 10% discount has been applied to your transaction`);
 } else if (customerType === "gold") {
-    total = subTotal * 0.05;
-    console.log(`Thank you for being a Platinum member! A 5% discount has been applied to your transaction: ${total}`);
+    extraDiscount = 0.05;
+    console.log(`Thank you for being a Platinum member! A 5% discount has been applied to your transaction`);
 } else if (customerType === "silver") {
-    total = subTotal * 0.02;
-    console.log(`Thank you for being a Platinum member! A 2% discount has been applied to your transaction: ${total}`);
+    extraDiscount = 0.02;
+    console.log(`Thank you for being a Platinum member! A 2% discount has been applied to your transaction`);
 } else if (customerType === "member") {
-    total = subTotal;
-    console.log(`Thank you for being a Member! Your total today is: ${total}`);
+    extraDiscount = 0;
+    console.log(`Thank you for being a Member!`);
 };
+
+//Step 5: Simulated Checkout Process & Inventory Updates
+for (subtotal = 0; subtotal < 3; subtotal++) {
+    console.log("subtotal" + subtotal)
+};
+//Step 6: Logging Key/Value Pairs
+for (let key in products[0]) {
+    console.log(key + ": " + products[0][key]);
+};
+//Step 7: Object.entries() and destructuring
